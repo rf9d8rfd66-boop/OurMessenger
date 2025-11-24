b1e851b59bdca74b242f6dc9dcb3f8da9248206c
h-happiness <tgalimkhanova@gmail.com>
2025-11-24 22:37:59 +0300

    Исправление ошибки при коммите

--- src/main/java/Group.java
+++ src/main/java/Group.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+public class Group{
+    private String name;
+    private Messanges lastMessenge;
+    private int countUsers;
 
-public class Group {
+
+    public Group(String name, Messanges lastMessenge, int countUsers) {
+        this.name = name;
+        this.lastMessenge = lastMessenge;
+        this.countUsers = countUsers;
+    }
+
+    public String toString() {
+        return "Group " + name + "\n" + lastMessenge + "\n" + "User counts = " + countUsers;
+    }
 }
--- src/main/java/Kanal.java
+++ src/main/java/Kanal.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Kanal {
+    private String name;
+    private Messanges lastMessenge;
+    private int countSub;
+
+    public Kanal(String name, Messanges lastMessenge, int countSub) {
+        this.name = name;
+        this.lastMessenge = lastMessenge;
+        this.countSub = countSub;
+    }
+
+    public String toString() {
+        return "Kanal " + name + "\n" + lastMessenge + "\n" + "Subscribers count = " + countSub + "\n" + "Subscribers: " + kanalSub;
+    }
 }
